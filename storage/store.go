0525e8c826471d4c4145bed5097c148665605216
upi <upi@localhost.localdomain>
2015-11-13 22:21:57 +0700

    initial commit

---
+++ storage/store.go
@@ -0,0 +1,16 @@
+package storage
+
+type SystemData struct {
+}
+
+type ApplicationData struct {
+}
+
+type Statistics struct {
+}
+
+type SystemMonitoringStore interface {
+}
+
+type ApplicationMonitoringStore interface {
+}
